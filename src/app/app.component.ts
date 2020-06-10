37091e2959a101359a046db87201639b7d947a80
annanemtseva <nemtseva.anna@gmail.com>
2020-06-10 20:45:42 +0300

    open categories by click, add animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import {Component, OnInit} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {Component, ElementRef, OnInit} from '@angular/core';
+import {HttpService} from './http.service';
+import {animate, state, style, transition, trigger} from '@angular/animations';
+
 
 export interface Tree {
   title: string;
@@ -10,22 +12,53 @@ export interface Tree {
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('openingCategories', [
+      state('start', style({
+        opacity: '0'
+      })),
+      state('end', style({
+        opacity: '1'
+      })),
+      transition(':enter', [
+        style({
+          opacity: '0'
+        }),
+        animate(300)
+      ]),
+      transition(':leave', [
+        style({
+          opacity: '1'
+        }),
+        animate(300)
+      ])
+    ])]
 })
 
 export class AppComponent implements OnInit {
   tree: Tree;
-  constructor(
-    private http: HttpClient,
-  ) {}
+  visibility = false;
+  isOpen = false;
+  openingCategories = 'start';
 
-  ngOnInit() {
-    this.http.get('../assets/tree7.json').subscribe((data: Tree) => {
-      this.tree = data;
-      // console.log(data);
-    }, (error) => {
-      console.log('error fetch json: ', error.message);
-    } );
+
+  constructor(
+    private httpService: HttpService,
+    private el: ElementRef
+  ) {
   }
 
+  ngOnInit() {
+    this.httpService.getData().subscribe((data: Tree) => {
+      this.tree = data;
+    }, (error) => {
+      console.log('Error fetch json: ', error.message);
+    });
+  }
+
+  openCategories() {
+    this.isOpen = !this.isOpen;
+    this.openingCategories = 'end';
+  }
 }
